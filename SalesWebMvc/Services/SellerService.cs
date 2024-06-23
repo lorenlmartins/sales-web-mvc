87c06aa424944fe12598f0a0926e1a844205f363
Loren Lima Martins <loren.lima@take.net>
2024-06-23 06:08:21 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -13,6 +13,7 @@ public class SellerService(SalesWebMvcContext context)
 
     public void Insert(Seller obj)
     {
+        obj.Department = _context.Department.First();
         _context.Add(obj);
         _context.SaveChanges();
     }
