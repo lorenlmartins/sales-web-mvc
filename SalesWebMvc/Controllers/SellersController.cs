ae5c465d9b007211cdeb96b4f5bfb2d7af23ceba
Loren Lima Martins <loren.lima@take.net>
2024-06-23 05:04:18 -0300

    SellersController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers;
+
+public class SellersController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
