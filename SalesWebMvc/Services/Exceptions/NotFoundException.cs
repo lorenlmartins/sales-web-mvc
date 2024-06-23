2341146e44441363b2f8f4f258ffccab1aafdc41
Loren Lima Martins <loren.lima@take.net>
2024-06-23 06:46:54 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc.Services.Exceptions;
+
+public class NotFoundException : ApplicationException
+{
+    public NotFoundException(string message) : base(message)
+    {
+    }
+}
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services;
 
@@ -28,5 +29,22 @@ public class SellerService(SalesWebMvcContext context)
         var obj = _context.Seller.Find(id);
         _context.Seller.Remove(obj);
         _context.SaveChanges();
+    }
+
+    public void Update(Seller obj)
+    {
+        if (!_context.Seller.Any(x => x.Id == obj.Id))
+        {
+            throw new NotFoundException("Id not found");
+        }
+        try
+        {
+            _context.Update(obj);
+            _context.SaveChanges();
+        }
+        catch (DbUpdateConcurrencyException e)
+        {
+            throw new DbConcurrencyException(e.Message);
+        }
     }
 }
