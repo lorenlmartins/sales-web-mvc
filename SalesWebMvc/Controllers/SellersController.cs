bd2d48f721074434ce60f29222860e111e062d06
Loren Lima Martins <loren.lima@take.net>
2024-06-23 06:53:34 -0300

    Returning custom error page

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -2,8 +2,7 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
-using SalesWebMvc.Services.Exceptions;
-
+using System.Diagnostics;
 namespace SalesWebMvc.Controllers;
 
 public class SellersController(SellerService sellerService, DepartmentService departmentService) : Controller
@@ -16,14 +15,12 @@ public class SellersController(SellerService sellerService, DepartmentService de
         var list = _sellerService.FindAll();
         return View(list);
     }
-
     public IActionResult Create()
     {
         var departments = _departmentService.FindAll();
         var viewModel = new SellerFormViewModel { Departments = departments };
         return View(viewModel);
     }
-
     [HttpPost]
     [ValidateAntiForgeryToken]
     public IActionResult Create(Seller seller)
@@ -31,23 +28,21 @@ public class SellersController(SellerService sellerService, DepartmentService de
         _sellerService.Insert(seller);
         return RedirectToAction(nameof(Index));
     }
-
     public IActionResult Delete(int? id)
     {
         if (id == null)
         {
-            return NotFound();
+            return RedirectToAction(nameof(Error), new { message = "Id not provided" });
         }
 
         var obj = _sellerService.FindById(id.Value);
         if (obj == null)
         {
-            return NotFound();
+            return RedirectToAction(nameof(Error), new { message = "Id not found" });
         }
 
         return View(obj);
     }
-
     [HttpPost]
     [ValidateAntiForgeryToken]
     public IActionResult Delete(int id)
@@ -55,61 +50,64 @@ public class SellersController(SellerService sellerService, DepartmentService de
         _sellerService.Remove(id);
         return RedirectToAction(nameof(Index));
     }
-
     public IActionResult Details(int? id)
     {
         if (id == null)
         {
-            return NotFound();
+            return RedirectToAction(nameof(Error), new { message = "Id not provided" });
         }
 
         var obj = _sellerService.FindById(id.Value);
         if (obj == null)
         {
-            return NotFound();
+            return RedirectToAction(nameof(Error), new { message = "Id not found" });
         }
 
         return View(obj);
     }
-
     public IActionResult Edit(int? id)
     {
         if (id == null)
         {
-            return NotFound();
+            return RedirectToAction(nameof(Error), new { message = "Id not provided" });
         }
 
         var obj = _sellerService.FindById(id.Value);
         if (obj == null)
         {
-            return NotFound();
+            return RedirectToAction(nameof(Error), new { message = "Id not found" });
         }
 
         List<Department> departments = _departmentService.FindAll();
         SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
         return View(viewModel);
     }
-
     [HttpPost]
     [ValidateAntiForgeryToken]
     public IActionResult Edit(int id, Seller seller)
     {
         if (id != seller.Id)
         {
-            return BadRequest();
+            return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
         }
         try
         {
             _sellerService.Update(seller);
             return RedirectToAction(nameof(Index));
         }
-        catch (NotFoundException)
+        catch (ApplicationException e)
         {
-            return NotFound();
+            return RedirectToAction(nameof(Error), new { message = e.Message });
         }
-        catch (DbConcurrencyException)
+    }
+
+    public IActionResult Error(string message)
+    {
+        var viewModel = new ErrorViewModel
         {
-            return BadRequest();
-        }
+            Message = message,
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        };
+        return View(viewModel);
     }
 }
