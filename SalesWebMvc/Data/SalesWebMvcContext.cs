678de89ec67b0cde9f42a7552e3460f7a21383f3
Loren Lima Martins <loren.lima@take.net>
2024-06-23 04:45:30 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -5,4 +5,6 @@ namespace SalesWebMvc.Models;
 public class SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options) : DbContext(options)
 {
     public DbSet<Department> Department { get; set; } = default!;
+    public DbSet<Seller> Seller { get; set; } = default!;
+    public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
 }
---
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc.Models.Enums;
+
+public enum SaleStatus
+{
+    Pending = 0,
+    Billed = 1,
+    Canceled = 2
+}
