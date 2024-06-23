01330a3de5eca5322341a417f90355714234764a
Loren Lima Martins <loren.lima@take.net>
2024-06-23 06:36:41 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -54,4 +54,20 @@ public class SellersController(SellerService sellerService, DepartmentService de
         _sellerService.Remove(id);
         return RedirectToAction(nameof(Index));
     }
+
+    public IActionResult Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var obj = _sellerService.FindById(id.Value);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+
+        return View(obj);
+    }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Services;
 
@@ -19,7 +20,7 @@ public class SellerService(SalesWebMvcContext context)
 
     public Seller FindById(int id)
     {
-        return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
     }
 
     public void Remove(int id)
